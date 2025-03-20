3cbaa415452159a88b3609b518e03130e448d390
Nakk2 <rattanakkhon@csus.edu>
2025-03-19 22:22:27 -0700

    Added UserManagement
    Added file for UserManagement tile in settings.

---
+++ client/src/pages/settings/UserManagement.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container, Typography } from "@mui/material";
+
+const UserManagement = () => {
+  return (
+    <Container sx={{ textAlign: "center", marginTop: 10 }}>
+      <Typography variant="h4">User Management</Typography>
+    </Container>
+  );
+};
+
+export default UserManagement;
