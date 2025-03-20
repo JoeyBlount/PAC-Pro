5dd488a1273d77b36af74e7654eafc2cbdf6a03d
Nakk2 <rattanakkhon@csus.edu>
2025-03-19 22:21:41 -0700

    Added StoreManagement
    Added StoreManagement file for the tile in settings.

---
+++ client/src/pages/settings/StoreManagement.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container, Typography } from "@mui/material";
+
+const StoreManagement = () => {
+  return (
+    <Container sx={{ textAlign: "center", marginTop: 10 }}>
+      <Typography variant="h4">Store Management</Typography>
+    </Container>
+  );
+};
+
+export default StoreManagement;
