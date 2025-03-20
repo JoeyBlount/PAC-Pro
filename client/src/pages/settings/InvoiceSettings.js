b50dda9dc77289986375bc3e48c3630db69033c6
Nakk2 <rattanakkhon@csus.edu>
2025-03-19 22:19:53 -0700

    Added InvoiceSettings
    File for InvoiceSettings tile on settings.

---
+++ client/src/pages/settings/InvoiceSettings.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container, Typography } from "@mui/material";
+
+const InvoiceSettings = () => {
+  return (
+    <Container sx={{ textAlign: "center", marginTop: 10 }}>
+      <Typography variant="h4">Invoice Settings</Typography>
+    </Container>
+  );
+};
+
+export default InvoiceSettings;
