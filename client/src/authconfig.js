32b4d392c3c5e46a22c97826509ef41340fd605f
mhameedcsus <mhameed@csus.edu>
2025-03-30 20:44:57 -0700

    microsoft works

--- client/src/authconfig.js
+++ client/src/authconfig.js
@@ -3,7 +3,7 @@ import { PublicClientApplication } from "@azure/msal-browser";
 export const msalConfig = {
     auth: {
       clientId: "3bcad4b5-5779-47ac-bf55-df702260d948",
-      authority: "https://login.microsoftonline.com/23eaf8b9-07a4-457f-b9ed-92f1ad5639c4",
+      authority: "https://login.microsoftonline.com/common",
       redirectUri: "http://localhost:3000",
     },
     cache: {
