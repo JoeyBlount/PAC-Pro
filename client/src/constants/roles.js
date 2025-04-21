2570017d1bb21049e602ed51c9a4bb77a61aa95d
Jamal Stanackzai <jstanack@gmail.com>
2025-04-20 23:36:22 -0700

    SCRUM-136
    Implement Roles and permissions

---
+++ client/src/constants/roles.js
@@ -0,0 +1,8 @@
+// src/constants/roles.js
+export const ROLES = {
+    ADMIN: 'Admin',
+    GENERAL_MANAGER: 'General Manager',
+    SUPERVISOR: 'Supervisor',
+    OFFICE_MANAGER: 'Office Manager',
+    ACCOUNTANT: 'Accountant',
+};
